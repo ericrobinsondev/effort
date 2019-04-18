2d608307667cf762701532e41785ddac89e41fb4
Eric Robinson <soyecr+dr@gmail.com>
2019-04-17 19:05:15 -0500

    Building client dir and serving react files

--- src/server.js
+++ src/server.js
@@ -18,8 +18,13 @@ app.use(morgan('dev'));
 app.use('/api', authRequired);
 app.post('/signup', signup);
 app.post('/signin', signin);
-app.get('/', (req, res) => {
-  res.status(200).end();
+// app.get('/', (req, res) => {
+//   res.status(200).end();
+// });
+
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 export const start = async () => {
