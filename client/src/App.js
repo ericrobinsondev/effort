85944cf4601a3be1a1989287d1fc4855d4588d4a
Eric Robinson <soyecr+dr@gmail.com>
2019-04-29 20:07:00 -0500

    Fixed build warnings

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import { Router, Link } from '@reach/router';
+import { Router } from '@reach/router';
 import './@uik/styles.css';
 import { Login } from './views/Login';
-import { Landing } from './views/Landing';
 import { AuthProvider } from './utils/AuthContext';
 import { ProtectedRoute } from './utils/ProtectedRoute';
 import { Layout } from './views/Layout';
@@ -13,9 +12,6 @@ class App extends Component {
     return (
       <div className='App'>
         <AuthProvider>
-          {/* <Link to='/'>Home</Link>
-          <Link to='/'>Landing</Link>
-          <Link to='/login'>Login</Link> */}
           <Router primary={false}>
             {/* <Home path='/' /> */}
             <ProtectedRoute path='/' component={Layout}>
