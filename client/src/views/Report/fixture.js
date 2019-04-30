80873680ddc603f534001e98365b7b8735c12c6a
eric robinson <46822577+drericrobinson@users.noreply.github.com>
2019-04-29 20:11:07 -0500

    Add group points chart to Report view (#6)
    * Added group points chart and fixture; mocked API
    
    * Fixed build warnings

---
+++ client/src/views/Report/fixture.js
@@ -0,0 +1,102 @@
+export const mockAPI = {
+  loadReportData: () => {
+    return {
+      id: 123,
+      userId: 4321,
+      startDate: {
+        year: 2019,
+        month: 4,
+        day: 21
+      },
+      pointsEarned: 0,
+      pointsExpected: 356,
+      questions: {
+        0: {
+          id: 0,
+          title: 'How many dials did you make?',
+          pointsEach: 5,
+          creditForEach: true,
+          pointsEarned: 0,
+          pointsExpected: 50,
+          comment: '',
+          amount: 0
+        },
+        1: {
+          id: 1,
+          title: 'How many names did you brainstorm?',
+          pointsEach: 1,
+          creditForEach: true,
+          pointsEarned: 0,
+          pointsExpected: 35,
+          comment: '',
+          amount: 0
+        },
+        2: {
+          id: 2,
+          title: 'Have you completed your admin work this week?',
+          pointsEach: 35,
+          creditForEach: false,
+          pointsEarned: 0,
+          pointsExpected: 35,
+          comment: '',
+          amount: 0
+        },
+        3: {
+          id: 3,
+          title: 'How many asks have you made this week?',
+          pointsEach: 15,
+          creditForEach: true,
+          pointsEarned: 0,
+          pointsExpected: 45,
+          comment: '',
+          amount: 0
+        },
+        4: {
+          id: 4,
+          title: 'How many thank you notes have you written this week?',
+          pointsEach: 5,
+          creditForEach: true,
+          pointsEarned: 0,
+          pointsExpected: 45,
+          comment: '',
+          amount: 0
+        },
+        5: {
+          id: 5,
+          title: 'How many pastors have you talked to this week?',
+          pointsEach: 7,
+          creditForEach: true,
+          pointsEarned: 0,
+          pointsExpected: 45,
+          comment: '',
+          amount: 0
+        }
+      },
+      reportComment: ''
+    };
+  },
+  loadGroupPoints: () => {
+    return {
+      0: {
+        name: 'Kristy',
+        currentWeekPoints: 343,
+        previousWeekPoints: 305
+      },
+      1: {
+        name: 'Tricia',
+        currentWeekPoints: 245,
+        previousWeekPoints: 300
+      },
+      2: {
+        name: 'Jason',
+        currentWeekPoints: 300,
+        previousWeekPoints: 123
+      },
+      3: {
+        name: 'Victoria',
+        currentWeekPoints: 25,
+        previousWeekPoints: 74
+      }
+    };
+  }
+};
