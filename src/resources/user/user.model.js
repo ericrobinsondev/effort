a1d627a6df63ae038e1bf22d69a680f50dacf672
Eric Robinson <soyecr+dr@gmail.com>
2019-05-02 21:42:28 -0500

    Updated user model

--- src/resources/user/user.model.js
+++ src/resources/user/user.model.js
@@ -41,7 +41,7 @@ const userSchema = new mongoose.Schema(
         ref: 'user'
       }
     ],
-    groupMember: {
+    group: {
       type: mongoose.SchemaTypes.ObjectId,
       ref: 'group'
     }
