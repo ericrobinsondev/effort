e524b1dcdc2cc4c136630785a06b1676cda902d1
Eric Robinson <soyecr+dr@gmail.com>
2019-05-30 15:54:09 -0500

    Added tests for date helpers

--- src/utils/__tests__/helpers.test.js
+++ src/utils/__tests__/helpers.test.js
@@ -1,4 +1,4 @@
-import { sendErrorMessage } from '../helpers';
+import { sendErrorMessage, weekStart, weekEnd, dateInWords } from '../helpers';
 
 describe('Helpers:', () => {
   describe('sendErrorMessage', () => {
@@ -37,6 +37,26 @@ describe('Helpers:', () => {
 
       await sendErrorMessage(error, res);
       expect(end).toHaveBeenCalled();
+    });
+  });
+
+  describe('Date functions', () => {
+    test('weekStart formats start of week correctly', () => {
+      const date = new Date(1559248763970);
+
+      expect(weekStart(date).toISOString()).toBe('2019-05-27T05:00:00.000Z');
+    });
+
+    test('weekEnd formats end of week correctly', () => {
+      const date = new Date(1559248763970);
+
+      expect(weekEnd(date).toISOString()).toBe('2019-06-03T04:59:59.999Z');
+    });
+
+    test('dateInWords formats date correctly', () => {
+      const date = new Date(1559248763970);
+
+      expect(dateInWords(date)).toBe('May 30');
     });
   });
 });
